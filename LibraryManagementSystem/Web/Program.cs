f1bde74837ab82c4dca7a22ae4650e59721850ae
Borislav Minkov <bobi.minkov112@gmail.com>
2024-11-20 08:10:40 +0200

    AddAdmin changes

--- LibraryManagementSystem/Web/Program.cs
+++ LibraryManagementSystem/Web/Program.cs
@@ -10,7 +10,7 @@ namespace Web
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+           
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
